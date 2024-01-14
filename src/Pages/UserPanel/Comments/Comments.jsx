caa21745947ee197765d024b89a43ed5c440e735
Milad salamian <miladsalami1385@gmail.com>
2024-01-14 01:14:53 -0800

    add page title to comment page in user panel

--- src/Pages/UserPanel/Comments/Comments.jsx
+++ src/Pages/UserPanel/Comments/Comments.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-
+import React from "react";
+import PageTitle from "../../../Components/UserPanel/PageTitle/PageTitle";
 function Comments() {
   return (
     <div>
-      Comments
+      <PageTitle title={'کامنت های من'} icon={'chat-bubble-left-right'} />
     </div>
-  )
+  );
 }
 
-export default Comments
+export default Comments;
