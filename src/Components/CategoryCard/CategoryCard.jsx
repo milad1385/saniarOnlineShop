fd1024c55e87002e229363063db3991b3c006c05
Milad salamian <miladsalami1385@gmail.com>
2026-06-21 15:47:11 +0330

    fix category card bug

--- src/Components/CategoryCard/CategoryCard.jsx
+++ src/Components/CategoryCard/CategoryCard.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function CategoryCard({category}) {
+function CategoryCard({ category }) {
   return (
-    <>
+    <Link to={`/category-products/${category.link}`}>
       <div className="bg-white flex items-center justify-between md:hover:-translate-y-[6px] p-3 md:p-4 rounded-md shadow-md transition-all">
         <div>
-          <img src={`https://saniar-backend.vercel.app/uploads/covers/${category.image}`} className="w-[100px] md:w-[130px]" />
+          <img
+            src={`https://saniar-backend.vercel.app/uploads/covers/${category.image}`}
+            className="w-[100px] md:w-[130px]"
+          />
         </div>
         <div className="flex items-center flex-col gap-y-4">
-          <Link to={`/category-products/${category.link}`} className="font-DanaMedium text-sm md:text-base">{category.title}</Link>
-          <Link to={`/category-products/${category.link}`}  className="flex items-center justify-between gap-x-2 cursor-pointer text-xs md:text-sm bg-gray-200 shadow px-2.5 py-1.5 border border-gray-300 hover:bg-white transition-colors rounded-md">
+          <Link
+            to={`/category-products/${category.link}`}
+            className="font-DanaMedium text-sm md:text-base"
+          >
+            {category.title}
+          </Link>
+          <Link
+            to={`/category-products/${category.link}`}
+            className="flex items-center justify-between gap-x-2 cursor-pointer text-xs md:text-sm bg-gray-200 shadow px-2.5 py-1.5 border border-gray-300 hover:bg-white transition-colors rounded-md"
+          >
             مشاهده
             <svg className="w-4 md:w-5 h-4 md:h-5 text-blue-600">
               <use href="#arrow-d"></use>
@@ -18,7 +29,7 @@ function CategoryCard({category}) {
           </Link>
         </div>
       </div>
-    </>
+    </Link>
   );
 }
 
